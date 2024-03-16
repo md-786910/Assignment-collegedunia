b26f3b3e0f99ac97a2dac66927553f2c7adc0a6b
md ashif reza <76996887+md-786910@users.noreply.github.com>
2024-03-16 21:15:57 +0530

    new api hosted apu

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const api = "http://localhost:5000/api/v1/users";
+const api = "https://assignment-collegedunia.onrender.com/api/v1/users";
+// const api = "http://localhost:5000/api/v1/users";
 
 const Axios = axios.create({
   baseURL: api,
